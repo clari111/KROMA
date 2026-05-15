0520735d20d3c9788213402c891b8c2488d40195
clari111 <136344430+clari111@users.noreply.github.com>
2026-05-15 08:24:34 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -2,3 +2,10 @@ import tensorflow as tf
 
 
 print ("tensorflow version: ", tf._version_)
+import tensorflow_datasets as tensorflow_datasets
+ 
+ datos, metadatos = tfds.load(
+    'fashion_mnist', 
+    as_superdvised=True, 
+    with_info=True
+    )
